1393dc2b223d0d708bfabb4a0a43058c401c8537
inteseptorialx <caiofigueiredo1510@gmail.com>
2025-10-08 16:24:10 -0300

    Att ThemeSwitcher
    add the correct style  fot the button

--- src/components/1-atoms/ThemeSwitcher.svelte
+++ src/components/1-atoms/ThemeSwitcher.svelte
@@ -21,10 +21,14 @@
   button {
     padding: 1rem;
     border-radius: 12px;
-    border: none;
+    border: solid 3px;
+    border-color: var(--borda-sutil);
     cursor: pointer;
     font-weight: 500;
-    background-color:  var( --texto-principal);
-    color: var(--fundo-secundario);
+    background-color:  var( --fundo-secundario);
+    color: var(--texto-secundario);
+  }
+  button:hover {
+    background-color: var(--fundo-hover);
   }
 </style>
