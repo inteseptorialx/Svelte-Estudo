5040df7678ec3c5c4252512da311ba52e3a9320f
inteseptorialx <caiofigueiredo1510@gmail.com>
2025-10-08 16:16:26 -0300

    adicionando o projeto ao repositório

---
+++ src/components/1-atoms/ThemeSwitcher.svelte
@@ -0,0 +1,30 @@
+<script>
+  import { theme } from '../../stores/themeStore.js';
+  
+
+  function toggleTheme() {
+    theme.update(currentValue => {
+      return currentValue === 'light' ? 'dark' : 'light';
+    });
+  }
+</script>
+
+<button on:click={toggleTheme}>
+  {#if $theme === 'light'}
+    Escuro
+  {:else}
+    Claro
+  {/if}
+</button>
+
+<style>
+  button {
+    padding: 1rem;
+    border-radius: 12px;
+    border: none;
+    cursor: pointer;
+    font-weight: 500;
+    background-color:  var( --texto-principal);
+    color: var(--fundo-secundario);
+  }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+  import '../app.css';
+  import { theme } from '../stores/themeStore.js';
+  import ThemeSwitcher from '../components/1-atoms/ThemeSwitcher.svelte';
+  import { browser } from '$app/environment';
+  
+  let { children } = $props();
+
+  $effect(() => {
+    if (browser) {
+      document.body.className = $theme;
+    }
+  });
+</script>
+
+<main>
+  {@render children()}
+</main>
+
+<ThemeSwitcher />
